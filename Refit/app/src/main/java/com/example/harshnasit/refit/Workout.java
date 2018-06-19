3a478fef315e61c40c970a3857300fcb798dc0a4
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-06-19 17:18:12 -0400

    Added methods addExercise and removeExercise.

--- Refit/app/src/main/java/com/example/harshnasit/refit/Workout.java
+++ Refit/app/src/main/java/com/example/harshnasit/refit/Workout.java
@@ -1,4 +1,22 @@
 package com.example.harshnasit.refit;
 
+import java.util.ArrayList;
+
 public class Workout {
+    private ArrayList<Exercise> exercises; // The exercises.
+
+    /** Add an exercise to this workout if its not in it.
+     * @param exercise The exercise to be added.
+     */
+    void addExercise(Exercise exercise){
+        if(!exercises.contains(exercise)) {
+            exercises.add(exercise);
+        }
+    }
+
+    void removeExercise(Exercise exercise){
+        if(exercises.contains(exercise)){
+            exercises.remove(exercise);
+        }
+    }
 }
