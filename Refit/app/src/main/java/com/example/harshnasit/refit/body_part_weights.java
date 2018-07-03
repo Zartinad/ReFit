90c9a6c4e480adba6716d539ee5b011b24704bf7
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-07-03 00:24:16 -0400

    same as previous commit

---
+++ Refit/app/src/main/java/com/example/harshnasit/refit/body_part_weights.java
@@ -0,0 +1,13 @@
+package com.example.harshnasit.refit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class body_part_weights extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_body_part_weights);
+    }
+}
