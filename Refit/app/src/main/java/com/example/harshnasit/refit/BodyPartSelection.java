429b4d314ad0d850f9e5d14c0a1f1533849a8be4
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-07-04 15:07:15 -0400

    When cardio button is clicked new intent opens new activity.

--- Refit/app/src/main/java/com/example/harshnasit/refit/BodyPartSelection.java
+++ Refit/app/src/main/java/com/example/harshnasit/refit/BodyPartSelection.java
@@ -15,13 +15,21 @@ public class BodyPartSelection extends AppCompatActivity {
         setContentView(R.layout.activity_body_part_selection);
 
         Button weightButton = findViewById(R.id.WeightButton);
-
+        Button cardioButton = findViewById(R.id.CardioButton);
         weightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(getApplicationContext(), body_part_weights.class);
                 startActivity(intent);
+            }
+        });
+
+        cardioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(), cardio_selection.class);
+                startActivity(intent1);
             }
         });
     }
---
+++ Refit/app/src/main/java/com/example/harshnasit/refit/cardio_selection.java
@@ -0,0 +1,13 @@
+package com.example.harshnasit.refit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cardio_selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardio_selection);
+    }
+}
