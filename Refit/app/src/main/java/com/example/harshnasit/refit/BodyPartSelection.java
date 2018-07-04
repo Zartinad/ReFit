c542fadeec8c085356cc6a7e2ef249b9b7e66ccf
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-07-04 11:59:10 -0400

    When weight button is clicked new intent opens new activity.

--- Refit/app/src/main/java/com/example/harshnasit/refit/BodyPartSelection.java
+++ Refit/app/src/main/java/com/example/harshnasit/refit/BodyPartSelection.java
@@ -1,7 +1,11 @@
 package com.example.harshnasit.refit;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BodyPartSelection extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class BodyPartSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body_part_selection);
+
+        Button weightButton = findViewById(R.id.WeightButton);
+
+        weightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), body_part_weights.class);
+                startActivity(intent);
+            }
+        });
     }
 }
